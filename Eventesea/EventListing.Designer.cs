53b008d361ec820522f130f0b905e05fde7a6616
RojenSoliman <rojen.soliman@lpunetwork.edu.ph>
2024-12-31 18:00:16 +0800

    Add files via upload

---
+++ Eventesea/EventListing.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Eventesea
+{
+    partial class EventListing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EventListing));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.eventName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.eventDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timeStart = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timeEnd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ticketNum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.eventID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnEventListing = new System.Windows.Forms.Button();
+            this.btnCalendar = new System.Windows.Forms.Button();
+            this.btnAnalysis = new System.Windows.Forms.Button();
+            this.btnManage = new System.Windows.Forms.Button();
+            this.btnNotif = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnUser = new System.Windows.Forms.Button();
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List of Your Events";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Location = new System.Drawing.Point(35, 186);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(827, 353);
+            this.panel1.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.eventName,
+            this.eventDate,
+            this.timeStart,
+            this.timeEnd,
+            this.ticketNum,
+            this.eventID});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(18, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(787, 297);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // eventName
+            // 
+            this.eventName.Text = "Event Name";
+            this.eventName.Width = 200;
+            // 
+            // eventDate
+            // 
+            this.eventDate.Text = "Event Date";
+            this.eventDate.Width = 70;
+            // 
+            // timeStart
+            // 
+            this.timeStart.Text = "Time Start";
+            this.timeStart.Width = 70;
+            // 
+            // timeEnd
+            // 
+            this.timeEnd.Text = "Time End";
+            this.timeEnd.Width = 70;
+            // 
+            // ticketNum
+            // 
+            this.ticketNum.Text = "Total Tickets";
+            this.ticketNum.Width = 100;
+            // 
+            // eventID
+            // 
+            this.eventID.Text = "ID";
+            this.eventID.Width = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.LightGray;
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Controls.Add(this.btnEventListing, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnCalendar, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnAnalysis, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnManage, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnNotif, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSettings, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnUser, 7, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pbxLogo, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 86F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(900, 86);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // btnEventListing
+            // 
+            this.btnEventListing.BackColor = System.Drawing.Color.LightGray;
+            this.btnEventListing.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnEventListing.FlatAppearance.BorderSize = 0;
+            this.btnEventListing.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEventListing.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEventListing.Location = new System.Drawing.Point(156, 4);
+            this.btnEventListing.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEventListing.Name = "btnEventListing";
+            this.btnEventListing.Size = new System.Drawing.Size(147, 78);
+            this.btnEventListing.TabIndex = 0;
+            this.btnEventListing.Text = "Event Listing";
+            this.btnEventListing.UseVisualStyleBackColor = false;
+            this.btnEventListing.Click += new System.EventHandler(this.btnEventListing_Click);
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.BackColor = System.Drawing.Color.LightGray;
+            this.btnCalendar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCalendar.FlatAppearance.BorderSize = 0;
+            this.btnCalendar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalendar.Location = new System.Drawing.Point(309, 4);
+            this.btnCalendar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Size = new System.Drawing.Size(147, 78);
+            this.btnCalendar.TabIndex = 1;
+            this.btnCalendar.Text = "Calendar";
+            this.btnCalendar.UseVisualStyleBackColor = false;
+            this.btnCalendar.Click += new System.EventHandler(this.btnCalendar_Click);
+            // 
+            // btnAnalysis
+            // 
+            this.btnAnalysis.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAnalysis.FlatAppearance.BorderSize = 0;
+            this.btnAnalysis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnalysis.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalysis.Location = new System.Drawing.Point(462, 4);
+            this.btnAnalysis.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAnalysis.Name = "btnAnalysis";
+            this.btnAnalysis.Size = new System.Drawing.Size(147, 78);
+            this.btnAnalysis.TabIndex = 2;
+            this.btnAnalysis.Text = "Analysis";
+            this.btnAnalysis.UseVisualStyleBackColor = true;
+            this.btnAnalysis.Click += new System.EventHandler(this.btnAnalysis_Click);
+            // 
+            // btnManage
+            // 
+            this.btnManage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnManage.FlatAppearance.BorderSize = 0;
+            this.btnManage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManage.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManage.Location = new System.Drawing.Point(615, 4);
+            this.btnManage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnManage.Name = "btnManage";
+            this.btnManage.Size = new System.Drawing.Size(147, 78);
+            this.btnManage.TabIndex = 3;
+            this.btnManage.Text = "Create Event";
+            this.btnManage.UseVisualStyleBackColor = true;
+            this.btnManage.Click += new System.EventHandler(this.btnManage_Click);
+            // 
+            // btnNotif
+            // 
+            this.btnNotif.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnNotif.Image = ((System.Drawing.Image)(resources.GetObject("btnNotif.Image")));
+            this.btnNotif.Location = new System.Drawing.Point(768, 4);
+            this.btnNotif.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnNotif.Name = "btnNotif";
+            this.btnNotif.Size = new System.Drawing.Size(39, 78);
+            this.btnNotif.TabIndex = 4;
+            this.btnNotif.Text = "button5";
+            this.btnNotif.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(813, 4);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(37, 29);
+            this.btnSettings.TabIndex = 5;
+            this.btnSettings.Text = "button6";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnUser
+            // 
+            this.btnUser.Location = new System.Drawing.Point(858, 4);
+            this.btnUser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(38, 29);
+            this.btnUser.TabIndex = 6;
+            this.btnUser.Text = "button7";
+            this.btnUser.UseVisualStyleBackColor = true;
+            // 
+            // pbxLogo
+            // 
+            this.pbxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbxLogo.Image")));
+            this.pbxLogo.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbxLogo.InitialImage")));
+            this.pbxLogo.Location = new System.Drawing.Point(3, 4);
+            this.pbxLogo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(145, 78);
+            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxLogo.TabIndex = 7;
+            this.pbxLogo.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 557);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Manage Selected Event";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EventListing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 614);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "EventListing";
+            this.Text = "Event Listing";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnEventListing;
+        private System.Windows.Forms.Button btnCalendar;
+        private System.Windows.Forms.Button btnAnalysis;
+        private System.Windows.Forms.Button btnManage;
+        private System.Windows.Forms.Button btnNotif;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnUser;
+        private System.Windows.Forms.PictureBox pbxLogo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader eventName;
+        private System.Windows.Forms.ColumnHeader eventDate;
+        private System.Windows.Forms.ColumnHeader timeStart;
+        private System.Windows.Forms.ColumnHeader timeEnd;
+        private System.Windows.Forms.ColumnHeader ticketNum;
+        private System.Windows.Forms.ColumnHeader eventID;
+    }
+}
